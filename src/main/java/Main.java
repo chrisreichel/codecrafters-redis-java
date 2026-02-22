562a95a9df3866146d4b4116f1a20de5141e0e3f
Christian Reichel <chrisreichel@Christians-Mac-mini.local>
2026-02-21 20:52:48 -0600

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Main {
+    private static final Map<String, String> store = new ConcurrentHashMap<>();
     public static void main(String[] args) {
         System.out.println("Logs from your program will appear here!");
 
@@ -41,6 +44,19 @@ public class Main {
                     } else if (command.equals("ECHO")) {
                         String arg = elements[1];
                         out.write(("$" + arg.length() + "\r\n" + arg + "\r\n").getBytes());
+                    } else if (command.equals("SET")) {
+                        String key = elements[1];
+                        String value = elements[2];
+                        store.put(key, value);
+                        out.write("+OK\r\n".getBytes());
+                    } else if (command.equals("GET")) {
+                        String key = elements[1];
+                        String value = store.get(key);
+                        if (value == null) {
+                            out.write("$-1\r\n".getBytes());
+                        } else {
+                            out.write(("$" + value.length() + "\r\n" + value + "\r\n").getBytes());
+                        }
                     }
                 }
             }
